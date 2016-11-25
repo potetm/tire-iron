fbba4969096c15b8f24c5a5dfafa7212bc8c8912
Tim Pote <tim.pote@cicayda.com>
2016-11-25 14:52:49 -0600

    Little bit of cleanup. Make sure we reset the *loaded-libs*.

--- src/prod/clj/com/potetm/tire_iron.clj
+++ src/prod/clj/com/potetm/tire_iron.clj
@@ -26,77 +26,6 @@
                       identity
                       repl-opts))
 
-(defn ns-parts [munged-ns]
-  (let [split (str/split (str munged-ns) #"\.")
-        part-count (count split)]
-    (loop [parts []
-           n 1]
-      (if (<= n part-count)
-        (recur (conj parts (str/join "."
-                                     (take n split)))
-               (inc n))
-        parts))))
-
-(defn compile-ns-exists-check [ns]
-  (str/join " && "
-            (map (fn [ns-part]
-                   (str "typeof " ns-part " !== 'undefined' "
-                        "&& " ns-part " !== null "))
-                 (ns-parts ns))))
-
-(defn compile-unload-ns [ns]
-  (let [ns (comp/munge ns)
-        loaded-libs (comp/munge 'cljs.core/*loaded-libs*)]
-    (str "(function(){\n"
-         "var ns, ns_string, path, key, value;\n"
-         "ns_string = \"" ns "\";\n"
-         "path = goog.dependencies_.nameToPath[ns_string];\n"
-         "goog.object.remove(goog.dependencies_.visited, path);\n"
-         "goog.object.remove(goog.dependencies_.written, path);\n"
-         "goog.object.remove(goog.dependencies_.written, goog.basePath + path);\n"
-         "if (" (compile-ns-exists-check ns) ") {\n"
-         "  ns = " ns ";\n"
-         "  for (key in ns) {\n"
-         "    if (!ns.hasOwnProperty(key)) { continue; }\n"
-         "    value = ns[key];"
-         ;; defmulti declares a defonce to hold its state. As noted below,
-         ;; we purposely set defonce'd members to null so they aren't redefined.
-         ;; Not redefining a multi causes defmethod failures on reload.
-         ;;
-         ;; Luckily, there is a handy `cljs.core/remove-all-methods` function to clear
-         ;; a multimethod's state. The trick then is to figure out which members
-         ;; are multimethods. The check below seems reliable.
-         "    if (typeof value.constructor !== 'undefined' && \n
-                    value.constructor.cljs$lang$ctorStr === 'cljs.core/MultiFn') {\n"
-         "      cljs.core.remove_all_methods.call(null, value);\n"
-         "    } else {\n"
-         ;; setting to null is critical to allow defonce to work as expected.
-         ;; defonce checks typeof var == "undefined"
-         ;; we need to allow certain vars to _not_ be redefined in order to
-         ;; maintain only one repl connection.
-         ;;
-         ;; I'm not certain, but it seemed like multiple connections usually
-         ;; resulted in stackoverflows in goog.require.
-         "      ns[key] = null;\n"
-         "    }\n"
-         "  }\n
-         }\n"
-         loaded-libs " = cljs.core.disj.call(null, " loaded-libs ", ns_string);\n"
-         "})();")))
-
-(defn compile-unload-nss [nss]
-  (str/join "\n"
-            (map compile-unload-ns
-                 nss)))
-
-(defn compile-load-nss [nss]
-  (str "goog.net.jsloader.loadMany([\n"
-       (str/join ",\n"
-                 (map (comp #(str "goog.basePath + goog.dependencies_.nameToPath[\"" % "\"]")
-                            comp/munge)
-                      nss))
-       "]);\n"))
-
 (defn eval-script [repl-env script]
   (let [{:keys [status value] :as ret}
         (repl/-evaluate repl-env
@@ -131,6 +60,111 @@
     (when is-self-require?
       (set! ana/*cljs-ns* restore-ns))))
 
+(defn object-path-parts [munged-path]
+  (let [split (str/split (str munged-path) #"\.")
+        part-count (count split)]
+    (loop [parts []
+           n 1]
+      (if (<= n part-count)
+        (recur (conj parts (str/join "."
+                                     (take n split)))
+               (inc n))
+        parts))))
+
+(defn parts-exist? [parts]
+  (str/join " && "
+            (map (fn [part]
+                   (str "typeof " part " !== 'undefined' "
+                        "&& " part " !== null "))
+                 parts)))
+
+(defn object-path-exists? [munged-path]
+  (parts-exist? (object-path-parts munged-path)))
+
+(defn call-sym-script [sym]
+  (when-not (str/blank? (str sym))
+    (str "if (" (object-path-exists? sym) ") {\n"
+         "  " sym ".call(null);\n"
+         "} else {\n"
+         "  throw new Error(\"Cannot resolve symbol: " sym "\");\n"
+         "}\n")))
+
+(defn set-ti-state-from-sym-script [state-sym]
+  (when-not (str/blank? (str state-sym))
+    (str "if (" (object-path-exists? state-sym) ") {\n"
+         "  goog.tire_iron_state_ = " state-sym ";\n"
+         "} else {\n"
+         "  throw new Error(\"Cannot resolve :state symbol: " state-sym "\");\n"
+         "}\n")))
+
+(defn set-sym-from-ti-state-script [state-sym]
+  (when-not (str/blank? (str state-sym))
+    (str "if (" (parts-exist? (butlast (object-path-parts state-sym))) ") {\n"
+         "  " state-sym " = goog.tire_iron_state_;\n"
+         "} else {\n"
+         "  throw new Error(\"Cannot resolve :state symbol: " state-sym "\");\n"
+         "}\n")))
+
+(defn compile-unload-ns [ns]
+  (let [ns (comp/munge ns)
+        loaded-libs (comp/munge 'cljs.core/*loaded-libs*)]
+    (str "(function(){\n"
+         "var ns, ns_string, path, key, value;\n"
+         "ns_string = \"" ns "\";\n"
+         "path = goog.dependencies_.nameToPath[ns_string];\n"
+         "goog.object.remove(goog.dependencies_.visited, path);\n"
+         "goog.object.remove(goog.dependencies_.written, path);\n"
+         "goog.object.remove(goog.dependencies_.written, goog.basePath + path);\n"
+         "if (" (object-path-exists? ns) ") {\n"
+         "  ns = " ns ";\n"
+         "  for (key in ns) {\n"
+         "    if (!ns.hasOwnProperty(key)) { continue; }\n"
+         "    value = ns[key];"
+         ;; defmulti declares a defonce to hold its state. As noted below,
+         ;; we purposely set defonce'd members to null so they aren't redefined.
+         ;; Not redefining a multi causes defmethod failures on reload.
+         ;;
+         ;; Luckily, there is a handy `cljs.core/remove-all-methods` function to clear
+         ;; a multimethod's state. The trick then is to figure out which members
+         ;; are multimethods. The check below seems reliable.
+         "    if (typeof value.constructor !== 'undefined' && \n
+                    value.constructor.cljs$lang$ctorStr === 'cljs.core/MultiFn') {\n"
+         "      cljs.core.remove_all_methods.call(null, value);\n"
+         "    } else {\n"
+         ;; setting to null is critical to allow defonce to work as expected.
+         ;; defonce checks typeof var == "undefined"
+         ;; we need to allow certain vars to _not_ be redefined in order to
+         ;; maintain only one repl connection.
+         ;;
+         ;; I'm not certain, but it seemed like multiple connections usually
+         ;; resulted in stackoverflows in goog.require.
+         "      ns[key] = null;\n"
+         "    }\n"
+         "  }\n
+         }\n"
+         loaded-libs " = cljs.core.disj.call(null, " loaded-libs ", ns_string);\n"
+         "})();")))
+
+(defn unload-nss-script [nss]
+  (str/join "\n"
+            (map compile-unload-ns
+                 nss)))
+
+(defn load-nss-browser-async [nss]
+  (let [loaded-libs (comp/munge 'cljs.core/*loaded-libs*)
+        nss-array (str "["
+                       (str/join ",\n"
+                                 (map (comp #(str "goog.basePath + goog.dependencies_.nameToPath[\"" % "\"]")
+                                            comp/munge)
+                                      nss))
+                       "]")]
+    (str "(function() {\n"
+         "var nss = " nss-array ";"
+         "return goog.net.jsloader.loadMany(nss).addCallback(function() {"
+         "  cljs.core.apply.call(null, cljs.core.conj, " loaded-libs ", nss);"
+         "  });"
+         "})();")))
+
 (defn remove-disabled [tracker]
   (-> tracker
       (update-in [::track/unload] (partial remove (set/union disabled-unload-namespaces
@@ -138,6 +172,8 @@
       (update-in [::track/load] (partial remove disabled-load-namespaces))))
 
 (defn refresh* [{:keys [repl-env
+                        analyzer-env
+                        repl-opts
                         source-dirs
                         before
                         after
@@ -153,41 +189,17 @@
   (prn :requesting-reload (::track/load refresh-tracker))
   (eval-script repl-env
                (str "(function() {\n"
-                    "if(typeof goog.net.jsloader == 'undefined') {\n"
-                    "  throw new Error(\"Tire iron not initialized\");"
-                    "}\n"
-                    (when before
-                      (let [b (comp/munge before)]
-                        (str "if (typeof " b " !== 'undefined') {\n"
-                             "  " b ".call(null);\n"
-                             "} else {\n"
-                             "  throw new Error(\"Cannot resolve :before symbol: " b "\");\n"
-                             "}\n")))
-                    (when state
-                      (let [s (comp/munge state)]
-                        (str "if (typeof " s " !== 'undefined') {\n"
-                             "  goog.tire_iron_state_ = " s ";\n"
-                             "} else {\n"
-                             "  throw new Error(\"Cannot resolve :state symbol: " s "\");\n"
-                             "}\n")))
-                    (compile-unload-nss (::track/unload refresh-tracker))
-                    "var d = " (compile-load-nss (::track/load refresh-tracker)) ";\n"
-                    (str "d.addCallback(function(res) {\n"
-                         (when state
-                           (let [s (comp/munge state)]
-                             (str "if (typeof " s " !== 'undefined') {\n"
-                                  "  " s " = goog.tire_iron_state_;\n"
-                                  "} else {\n"
-                                  "  throw new Error(\"Cannot resolve :state symbol: " s "\");\n"
-                                  "}\n")))
-                         (when after
-                           (let [a (comp/munge after)]
-                             (str "if (typeof " a " !== 'undefined') {\n"
-                                  "  " a ".call(null);\n"
-                                  "} else {\n"
-                                  "  throw new Error(\"Cannot resolve :before symbol: " a "\");\n"
-                                  "}\n")))
-                         "})\n")
+                    (call-sym-script (comp/munge before))
+                    (set-ti-state-from-sym-script (comp/munge state))
+                    (unload-nss-script (map comp/munge
+                                            (::track/unload refresh-tracker)))
+                    "var d = " (load-nss-browser-async (map comp/munge
+                                                            (::track/load refresh-tracker)))
+                    ";\n"
+                    "d.addCallback(function(ret) {\n"
+                    (set-sym-from-ti-state-script (comp/munge state))
+                    (call-sym-script (comp/munge after))
+                    "  });\n"
                     "})();"))
   (alter-var-root #'refresh-tracker (fn [tracker]
                                       (assoc tracker
@@ -213,13 +225,14 @@
     (second symbol-or-list)
     symbol-or-list))
 
-(defn disable-unload!
-  ([ns]
-   (alter-var-root #'disabled-unload-namespaces conj (maybe-quoted->symbol ns))))
+(defn disable-unload! [ns]
+  (alter-var-root #'disabled-unload-namespaces conj (maybe-quoted->symbol ns)))
 
-(defn disable-reload!
-  ([ns]
-   (alter-var-root #'disabled-load-namespaces conj (maybe-quoted->symbol ns))))
+(defn disable-reload! [ns]
+  (alter-var-root #'disabled-load-namespaces conj (maybe-quoted->symbol ns)))
+
+(defn clear! []
+  (alter-var-root #'refresh-tracker (constantly (track/tracker))))
 
 (defn print-disabled []
   (prn "disabled unload" disabled-unload-namespaces)
@@ -282,6 +295,13 @@
                                :analyzer-env analyzer-env
                                :repl-opts repl-opts})))))
 
+   'init
+   (wrap (fn [repl-env analyzer-env _ repl-opts]
+           (require-lib repl-env
+                        analyzer-env
+                        repl-opts
+                        'goog.net.jsloader)))
+
    'print-tis
    (wrap (fn [repl-env analyzer-env _ repl-opts]
            (print-state repl-env
@@ -308,14 +328,6 @@
    (wrap (fn [_ _ [_ my-ns] _]
            (disable-reload! my-ns)))
 
-   'init
-   (wrap (fn [repl-env analyzer-env _ repl-opts]
-           (require-lib repl-env
-                        analyzer-env
-                        repl-opts
-                        'goog.net.jsloader)))
-
    'clear
    (wrap (fn [_ _ _ _]
-           (alter-var-root #'refresh-tracker
-                           (constantly (track/tracker)))))})
+           (clear!)))})
